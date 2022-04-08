3de9f42214f7bdbe5231de9ecf38d726468e7f29
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-04-08 14:19:39 -0300

    remove verify/v2/

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/token/verify/v2/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),    
 ]
