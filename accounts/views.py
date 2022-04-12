52fb69a5ccf03f4ad7fa6df127a7efc94b9e4c59
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-04-12 09:29:41 -0300

    add users/ and users/me apis

--- accounts/views.py
+++ accounts/views.py
@@ -1,53 +1,90 @@
-from rest_framework.serializers import ModelSerializer
-from rest_framework.viewsets import ModelViewSet, ViewSet, GenericViewSet
+from rest_framework import serializers
 from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdateAPIView
 from .models import User
-from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.hashers import make_password
 
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-
-
-class UserWriteSerializer(ModelSerializer):
+class UserOutputSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'email',
-            'full_name',
-            'city',
-            'state',
-            'gender',
-            'sector',
-            'password',
+            "id",
+            "email",
+            "full_name",
+            "city",
+            "state",
+            "gender",
+            "sector",
+            "last_login",
+            "date_joined",
         ]
 
 
-UserRetrieveSerializer = UserWriteSerializer
-UserUpdateSerializer = UserWriteSerializer
+class UserInputSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "full_name",
+            "city",
+            "state",
+            "gender",
+            "sector",
+        ]
 
 
-class MultiSerializerModelViewSet(object):
+class MultiSerializerModelView(object):
     serializers = {}
 
     def get_serializer_class(self):
-        return self.serializers.get(self.action, self.serializer_class)
+        action = self.request.method.lower()
+        return self.serializers.get(action, self.serializer_class)
 
 
-class UsersMeViewSet(
-    MultiSerializerModelViewSet,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    GenericViewSet,
+class UsersMeView(
+    MultiSerializerModelView,
+    UpdateAPIView,
+    RetrieveUpdateAPIView
 ):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserOutputSerializer
     permission_classes = [IsAuthenticated]
     serializers = {
-        'retrieve': UserRetrieveSerializer,
-        'update': UserUpdateSerializer,
-        'partial_update': UserUpdateSerializer,
+        'retrieve': UserOutputSerializer,
+        'update': UserInputSerializer,
+        'partial_update': UserInputSerializer,
     }
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserCreateInputSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "password",
+            "full_name",
+            "city",
+            "state",
+            "gender",
+            "sector",
+        ]
+
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'password': {'write_only': True},
+        }
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+
+class UsersView(CreateAPIView):
+    authentication_classes = []
+    serializer_class = UserCreateInputSerializer
