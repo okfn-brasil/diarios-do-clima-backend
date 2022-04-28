a7f2a0ba0d3fc40165518e6a596a09d2bf487e64
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-04-28 16:18:16 -0300

    add to_charge method

--- plans/models.py
+++ plans/models.py
@@ -6,7 +6,7 @@ class Plan(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     title = models.CharField(max_length=255)
     html = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=8)
+    price = models.DecimalField(decimal_places=2, max_digits=8) # remover
     pagseguro_plan_id = models.CharField(
         max_length=255,
         unique=True,
@@ -18,3 +18,8 @@ class Plan(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def to_charge(self) -> bool:
+        has_pag_plan = self.pagseguro_plan_id is not None
+        has_non_zero_price = self.price > 0
+        return has_pag_plan and has_non_zero_price
