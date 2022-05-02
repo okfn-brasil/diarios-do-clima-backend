e6f5296c2f44b7d939d68c4bc8e638c93d67ba03
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-05-02 19:18:38 -0300

    add notifications webhook

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,10 +1,12 @@
 from django.db import transaction
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
-from .models import PlanSubscriptionStatus, Plan
+from rest_framework.response import Response
+from .models import PlanSubscription, PlanSubscriptionStatus, Plan
 from .serializers import PlanSubscriptionSerializer
 from libs.services import services
-from libs.pagseguro import PagSeguroApiABC
+from libs.pagseguro import PagSeguroApiABC, PreApprovalNotification
 from libs.pagseguro.serializers import SubscribeSerializer
 
 
@@ -15,6 +17,39 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+class NotificationsApiView(APIView):
+    notification_type = 'preApproval'
+
+    def post(self, request, *args, **kwargs):
+        notification_code = request.POST.get('notificationCode')
+        notification_type = request.POST.get('notificationType')
+
+        if notification_type != self.notification_type:
+            return Response()
+
+        pag_seguro_api: PagSeguroApiABC = services.get(PagSeguroApiABC)
+        notification = pag_seguro_api.pre_approvals_get_notification(
+            notification_code=notification_code)
+
+        plan_subscription = PlanSubscription.objects.get(
+            pagseguro_code=notification.code,
+        )
+
+        try:
+            PlanSubscriptionStatus.objects.get(
+                pagseguro_notification_code=notification_code)
+        except PlanSubscriptionStatus.DoesNotExist:
+            plan_subscription_status = PlanSubscriptionStatus(
+                plan_subscription=plan_subscription,
+                pagseguro_data=notification.status,
+                pagseguro_notification_code=notification_code,
+            )
+
+            plan_subscription_status.save()
+
+        return Response()
 
 
 class PlanSubscriptionApiView(CreateAPIView):
