edac55303c234ea5b17b032797fb66f72b3a0b32
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-05-05 17:32:48 -0300

    add plan subscription statusto admin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -19,3 +19,19 @@ class PlanSubscriptionAdmin(admin.ModelAdmin):
     inlines = (
         PlanSubscriptionStatusInline,
     )
+
+@admin.register(PlanSubscriptionStatus)
+class PlanSubscriptionStatus(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'pagseguro_data',
+        'pagseguro_notification_code'
+    )
+
+    list_filter = (
+        'pagseguro_data',
+    )
+
+    search_fields = (
+        'pagseguro_notification_code',
+    )
