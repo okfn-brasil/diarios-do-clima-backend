c96093620815415873da35d597973048a39d4d0b
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-05-25 14:39:05 -0300

    set trial_end_at when creating a subscription

--- subscriptions/views/plan_subscription_api_view.py
+++ subscriptions/views/plan_subscription_api_view.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.utils import timezone
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView
 from ..models import PlanSubscription, PlanSubscriptionStatus, Plan
@@ -9,7 +10,6 @@ from libs.services import services
 from libs.pagseguro import PagSeguroApiABC
 from libs.pagseguro.serializers import SubscribeSerializer
 from libs.get_client_ip import get_client_ip
-
 
 
 class PlanSubscriptionApiView(CreateAPIView):
@@ -67,13 +67,22 @@ class PlanSubscriptionApiView(CreateAPIView):
             serializer=subscribe_serializer
         )
         plan_subscription.pagseguro_code = code
-        plan_subscription.save()
+
+    def set_trial_end(self, plan_subscription: PlanSubscription) -> None:
+        plan: Plan = plan_subscription.plan
+        if not plan.to_charge():
+            return
+        today = timezone.datetime.today()
+        time_delta = timezone.timedelta(days=plan.trial_days)
+        trial_end_at = today + time_delta
+        plan_subscription.trial_end_at = trial_end_at
 
     def perform_create(self, serializer):
         with transaction.atomic():
             self.previous_plan_subscription_cancel()
             plan_subscription = serializer.save(user=self.request.user)
-            plan_subscription.save()
+            self.set_trial_end(plan_subscription=plan_subscription)
             self.new_plan_subscription_create(
                 plan_subscription=plan_subscription,
             )
+            plan_subscription.save()
