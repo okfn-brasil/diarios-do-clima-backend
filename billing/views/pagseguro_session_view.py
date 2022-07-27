813208ab5b38b7bc8cffe24069babc04ca837899
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-07-27 14:52:15 -0300

    trata erro de session

--- billing/views/pagseguro_session_view.py
+++ billing/views/pagseguro_session_view.py
@@ -5,19 +5,20 @@ from rest_framework.exceptions import APIException
 
 
 from libs.pagseguro import PagSeguroApiABC
+from libs.pagseguro.exceptions import GenericSessionError
 from libs.services import services
 
 
-class PagSeguroSessionView(APIView):
+class PagSeguroSessionView(APIView):    
     permission_classes = [IsAuthenticated]
 
     def get(self, request, **kwargs):
         pag_seguro_api: PagSeguroApiABC = services.get(PagSeguroApiABC)
-        
+
         try:
             session = pag_seguro_api.get_session()
-        except:
-            raise APIException()
+        except GenericSessionError:
+            raise APIException("erro generating session")
 
         return Response(
             data={
