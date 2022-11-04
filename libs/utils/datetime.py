25c4f4d4fe2f1924a340129587e6b0efc6df1f50
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-11-04 12:46:57 -0300

    add datetime_to_datetime_str_diario

--- libs/utils/datetime.py
+++ libs/utils/datetime.py
@@ -1,9 +1,10 @@
 from django.utils import timezone
 
 
-
 date_format_default = "%d/%m/%Y"
 date_format_diario = "%Y-%m-%d"
+
+datetime_format_diario = "%Y-%m-%dT%H:%M:%S"
 
 
 def datetime_to_date_str(date: timezone.datetime) -> str:
@@ -12,6 +13,10 @@ def datetime_to_date_str(date: timezone.datetime) -> str:
 
 def datetime_to_date_str_diario(date: timezone.datetime) -> str:
     return date.strftime(date_format_diario)
+
+
+def datetime_to_datetime_str_diario(date: timezone.datetime) -> str:
+    return date.strftime(datetime_format_diario)
 
 
 def datetime_from_date_str(date: str) -> timezone.datetime:
