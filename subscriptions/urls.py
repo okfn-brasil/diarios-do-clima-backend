477e94c63114da026f15bcedda90609221a34525
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2022-12-07 15:10:51 -0300

    add subscription orders api

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,10 +3,12 @@ from .views import (
     NotificationsApiView,
     PlanSubscriptionApiView,
     PlanSubscriptionCancelAPIView,
+    PlanSubscriptionOrdersApiView,
 )
 
 urlpatterns = [
     path('', PlanSubscriptionApiView.as_view(), name='subscriptions'),
     path('cancel/', PlanSubscriptionCancelAPIView.as_view(), name='subscriptions_cancel'),
+    path('orders/<int:pk>/', PlanSubscriptionOrdersApiView.as_view(), name='orders'),
     path('notifications/', NotificationsApiView.as_view(), name='notifications'),
 ]
