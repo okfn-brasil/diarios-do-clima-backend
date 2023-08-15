58245a63742b3c551a9c8b0b6811fa18c22cb76c
Giulio Carvalho <giulioccavalcante@gmail.com>
2023-08-15 17:14:28 -0300

    Modifica mensagem de assunto de redefinição de senha

--- app/accounts/signals.py
+++ app/accounts/signals.py
@@ -1,15 +1,12 @@
-from django.conf import settings
-from libs.utils.email import Email, send_email
 from django.apps import AppConfig
-from django.db.models.signals import post_save
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
+from plans.actions import create_user_default_plan_subscription
 
-from plans.actions import (
-    create_user_default_plan_subscription
-)
+from libs.utils.email import Email, send_email
 
 
 def user_post_save_default_subscription(sender, instance, created, **kwargs):
@@ -27,7 +24,9 @@ def init_signals(app: AppConfig):
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+def password_reset_token_created(
+    sender, instance, reset_password_token, *args, **kwargs
+):
     uri = "/redefinir-senha"
     key = reset_password_token.key
     reset_password_url = f"{settings.FRONT_BASE_URL}{uri}?token={key}"
@@ -39,7 +38,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
     send_email(
         Email(
-            subject="Diário do Clima - Redefinição de senha",
+            subject=f"[{settings.PROJECT_TITLE}] Redefinição de senha",
             email_to=[reset_password_token.user.email],
             message=message,
         )
